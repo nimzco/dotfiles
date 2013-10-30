c47d12e31265105a5113288e95333b11e2dd2a90
Nicolas Dupont <npg.dupont@gmail.com>
2013-10-30 20:42:32 +0100

    Add setup for brew

---
+++ setup/brew.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install homebrew
+if ! which brew &> /dev/null
+then
+	ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
+
+# Prepare brew for multi-users setup
+if ! sudo dscl . read /Groups/brew &> /dev/null
+then
+	echo "Now let's make brew useful for all users."
+	echo "Create a 'brew' group"
+	sudo dscl . create /Groups/brew
+	sudo dscl . create /Groups/brew RealName "Homebrew group"
+	sudo dscl . create /Groups/brew gid 400
+	sudo dscl . create /Groups/brew GroupMembership `whoami`
+fi
+
+echo -n "Change the group of homebrew cache directory…"
+sudo chgrp -R brew /Library/Caches/Homebrew
+echo "done"
+
+echo -n "Allow group members to write inside this directory…"
+sudo chmod -R g+w /Library/Caches/Homebrew
+echo "done"
+
