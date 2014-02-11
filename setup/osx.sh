6c00e8fad44ac9df7450088daf09d7b89b8a102f
Nima Izadi <nim.izadi@gmail.com>
2014-02-11 18:49:43 +0100

    Updated Environment settings

--- setup/osx.sh
+++ setup/osx.sh
@@ -276,7 +276,7 @@ defaults write com.apple.dock launchanim -bool false
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
 # Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
+# defaults write com.apple.dashboard mcx-disabled -bool true
 
 # Don’t show Dashboard as a Space
 defaults write com.apple.dock dashboard-in-overlay -bool true
